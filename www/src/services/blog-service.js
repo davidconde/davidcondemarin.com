f15ae75b01656aa3ad63c0229093e516208d01f3
David Conde <david.conde.marin@gmail.com>
2024-11-23 21:44:40 +0000

    fetching posts from API instead

--- www/src/services/blog-service.js
+++ www/src/services/blog-service.js
@@ -1,45 +1,14 @@
-let Parser = require('rss-parser');
-const { DateTime } = require("luxon");
 const axios = require("axios");
-
-let parser = new Parser();
-
-const fetchCount = 4;
-const genericImage = "/img/blog_post.jpg";
-
-const getPostImage = (postContent) => {
-  if (!postContent)
-    return genericImage;
-
-  const regex = new RegExp("<img src=\"(.+?)\"");
-  const matches = regex.exec(postContent);
-
-  if (!matches || matches.length === 0)
-    return genericImage;
-  
-  return matches[1];  
-}
-
-const mapStory = (post) => {
-
-  const date = DateTime.fromISO(post.isoDate);
-  const formattedDate = date.toFormat("dd LLL yyyy");
-  const postImage = getPostImage(post.content);
-
-  return {
-    title: post.title,
-    date: formattedDate,
-    link: post.link,
-    image: postImage
-  };
-}
-
-
+const BASE_URL = process.env.REACT_APP_BASE_API_PATH;
 
 const fetchLatestBlogPosts = async () => {
-  const rssFeed = await axios.get("http://localhost:3000/blog-posts");
+  const fetchUrl = `${BASE_URL}/blog-posts`;
+  console.log(fetchUrl)
 
-  if (!rssFeed.data || !rssFeed.data.response)
+  const rssFeed = await axios.get(fetchUrl);
+  
+
+  if (!rssFeed || !rssFeed.data || !rssFeed.data.response)
     return [];
 
   return rssFeed.data.response;
